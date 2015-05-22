6777bbc340723af95dbd46d4272606d46eb25fac
Nicolas Silva <nsilvah@gmail.com>
2015-05-22 00:59:04 -0300

    Se implementa cambio de viz entre area y bar

--- bb/junarData.js
+++ bb/junarData.js
@@ -105,13 +105,24 @@ var junarDataModel = Backbone.Model.extend({
 
 var junarDataView = Backbone.View.extend({
 	initialize: function() {
-      this.listenToOnce(this.model, 'change:the_matrix', this.render);
+
+        this.listenTo(this.model, 'change:the_matrix change:chart_type', function(){
+            var chartType = this.model.get('chart_type');
+            var matrix = this.model.get('the_matrix');
+
+            if(chartType == 'bar')
+                this.renderBar(matrix);
+            else
+                this.render(matrix);
+        });
+
+
     },
 	el: '#d3Chart',
-    render: function(){
+    render: function(matrix){
 
-        var data = this.model.get('the_matrix');
-        var headers = data.shift();
+        var data = matrix.slice(1);
+        var headers = matrix[0];
 
         var $el = this.$el.empty();
 
@@ -241,11 +252,11 @@ var junarDataView = Backbone.View.extend({
 
         update(data,headers);
 
-        this.listenTo(this.model, 'change:the_matrix', function(){
-            var data = this.model.get('the_matrix');
-            var headers = data.shift();
-            update(data,headers);
-        });
+        //this.listenTo(this.model, 'change:the_matrix', function(){
+        //    var data = this.model.get('the_matrix');
+        //    var headers = data.shift();
+        //    update(data,headers);
+        //});
 
 
         var legend = svg.append("g")
@@ -314,5 +325,231 @@ var junarDataView = Backbone.View.extend({
             }
 
         }
+    },
+
+    renderBar: function(matrix){
+
+        var data = matrix.slice(1);
+        var headers = matrix[0];
+
+        var $el = this.$el.empty();
+
+
+        var margin = {top: 20, right: 20, bottom: 30, left: 80},
+            width = $el.width() - margin.left - margin.right,
+            height = $el.height() - margin.top - margin.bottom;
+
+        var formatter = d3.format(',');
+
+        var color = d3.scale.category10();
+
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1);
+
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .tickSize(-width);
+
+
+        var area = d3.svg.area()
+            .x(function (d) {
+                return x(d.x);
+            })
+            .y0(function (d) {
+                return y(d.y0);
+            })
+            .y1(function (d) {
+                return y(d.y0 + d.y);
+            });
+
+        var stack = d3.layout.stack()
+            .values(function (d) {
+                return d.values;
+            });
+
+        var svg = d3.select($el.get(0)).append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .style('overflow', 'visible')
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+        //.call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            //.call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr('x', -(height / 2))
+            .attr("y", -margin.left)
+            .attr("dy", ".8em")
+            .style("text-anchor", "middle")
+            .text("Budget Amount");
+
+        var g = svg.append('g');
+
+        var legend = svg.append("g")
+            .attr("class","legend")
+            .attr("transform","translate(10,0)")
+            .style("font-size","12px");
+
+        var sectores = function (data,headers) {
+            var result = [];
+
+            for (var i = 1; i < data[0].length; i++){
+                result.push({
+                    id: i,
+                    name: headers[i],
+                    values: data.map(function(d){
+                        return {x: d[0], y:d[i]}
+                    })
+                });
+            }
+
+            return stack(result);
+
+
+        };
+
+
+        var stackedData;
+        var update = function (data,headers) {
+
+            x.domain(data.map(function(d){return d[0]}))
+            y.domain([0,d3.max(data,function(d){return d3.sum(d,function(d,i){return i==0?0:d})})]);
+
+            svg.select(".y.axis").call(yAxis);
+            svg.select(".x.axis").call(xAxis);
+
+
+            stackedData = sectores(data,headers);
+
+            g.selectAll('*').remove();
+
+            var paths = g.selectAll(".sector")
+                .data(stackedData);
+
+            paths.enter().append('g');
+
+            paths
+                .attr('class', 'sector')
+                .attr('fill', function (d) {
+                    return color(d.id)
+                })
+                .attr("data-legend",function(d) { return d.name});
+
+
+            paths.selectAll('rect')
+                .data(function(d){return d.values})
+                .enter().append('rect')
+                .attr('x', function(d){return x(d.x) })
+                .attr('y',function(d){return y(d.y0) - height + y(d.y)})
+                .attr('width', x.rangeBand())
+                .attr('height', function(d){return height-y(d.y)})
+                .attr('fill-opacity', 0.8);
+
+                //.each('end',function(){
+                //    legend.call(d3.legend)
+                //});
+
+            paths.exit().remove();
+
+            legend.call(d3.legend);
+
+
+
+        }
+
+        update(data,headers);
+
+        //this.listenTo(this.model, 'change:the_matrix', function(){
+        //    var data = this.model.get('the_matrix');
+        //    var headers = data.shift();
+        //    update(data,headers);
+        //});
+
+
+
+
+
+        /*
+
+        var focus = svg.append("g")
+            .attr("class", "focus")
+            .style("display", "none");
+
+        focus.append("circle")
+            .attr("r", 6)
+            .style('fill-opacity', 0.5);
+
+        focus.append('rect')
+            .attr('fill', '#333')
+            .attr('width', 180)
+            .attr('height', 25)
+            .attr('x', -90)
+            .attr('y', -38);
+
+        focus.append("text")
+            .attr('fill', '#fff')
+            .attr('text-anchor', 'middle')
+            .attr("y", -20);
+
+        svg.append("rect")
+            .attr("class", "overlay")
+            .attr("width", width)
+            .attr("height", height)
+            .style('fill', 'none')
+            .style('pointer-events', 'all')
+            .on("mouseover", function () {
+                focus.style("display", null);
+            })
+            .on("mouseout", function () {
+                focus.style("display", "none");
+            })
+            .on("mousemove", mousemove);
+
+        function mousemove() {
+
+            var x0 = d3.mouse(this)[0];
+            var y0 = d3.mouse(this)[1];
+            var minDistance = Number.MAX_VALUE;
+            var d = null;
+
+            for (var i in stackedData) {
+                for (var j in stackedData[i].values) {
+                    var distance = Math.pow(x0 - x(stackedData[i].values[j].x), 2) + Math.pow(y0 - y(stackedData[i].values[j].y0 + stackedData[i].values[j].y), 2);
+                    if (distance <= Math.pow(80, 2) && distance < minDistance) {
+                        minDistance = distance;
+                        d = stackedData[i].values[j];
+                    }
+                }
+            }
+
+            if (d) {
+                focus.style("display", null);
+                focus.attr("transform", "translate(" + x(d.x) + "," + y(d.y0 + d.y) + ")");
+                focus.select("text").text(formatter(d.y0 + d.y) + ' Budget Amount - ' + d.x.getFullYear());
+            } else {
+                focus.style("display", "none");
+            }
+
+        }
+        */
     }
+
 });
